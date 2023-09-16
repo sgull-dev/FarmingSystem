1c059b57d0913409ec5e4e4a66953f5c06a9f83a
sgull-dev <122727788+sgull-dev@users.noreply.github.com>
2023-09-16 10:17:10 +0300

    Grass

--- scripts/terrain/block_ground.gd
+++ scripts/terrain/block_ground.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-enum GROUND_MODE {DIRT, TILLED}
+enum GROUND_MODE {DIRT, TILLED, GRASS}
 
 var ground_mode = GROUND_MODE.DIRT
 var plant_type = PlantDatabase.PLANT_TYPE.NONE
@@ -10,10 +10,37 @@ var is_block = true
 @onready var graphic = $Node3D/ground_plane/Plane
 
 
+func make_grass():
+	ground_mode = GROUND_MODE.GRASS
+	graphic.material_override = load("res://materials/block_grass.tres")
+	graphic.material_overlay = null
+
+
 func till_block():
 	print("Tilling block.")
 	ground_mode = GROUND_MODE.TILLED
 	graphic.material_override = load("res://materials/block_tilled.tres")
+	graphic.material_overlay = null
+	add_till_overlay_to_neighboring_blocks()
+
+
+func add_till_overlay_to_neighboring_blocks():
+	var str = name
+	var x = int(str.get_slice("_", 1))
+	var y = int(str.get_slice("_", 2))
+	print(x,y)
+	#top neighbor
+	var top_n_name = "Block_" + str(x-1) + "_" + str(y)
+	var top_block = GameData.get_current_stage().get_node("GroundGenerator/" + top_n_name)
+	if top_block != null:
+		if top_block.ground_mode == GROUND_MODE.DIRT:
+			top_block.get_node("Node3D/ground_plane/Plane").material_overlay = load("res://materials/block_overlay_till_top.tres")
+	#bottom neighbor
+	var bot_n_name = "Block_" + str(x+1) + "_" + str(y)
+	var bot_block = GameData.get_current_stage().get_node("GroundGenerator/" + bot_n_name)
+	if bot_block != null:
+		if bot_block.ground_mode == GROUND_MODE.DIRT:
+			bot_block.get_node("Node3D/ground_plane/Plane").material_overlay = load("res://materials/block_overlay_till_bottom.tres")
 
 
 func add_plant(type):
@@ -30,3 +57,4 @@ func reset_block():
 	ground_mode = GROUND_MODE.DIRT
 	plant_type = PlantDatabase.PLANT_TYPE.NONE
 	graphic.material_override = load("res://materials/block_ground.tres")
+	graphic.material_overlay = null
--- scripts/terrain/ground_generator.gd
+++ scripts/terrain/ground_generator.gd
@@ -8,12 +8,18 @@ var _block = preload("res://scenes/terrain/block_ground.tscn")
 
 #vars for spawning trees
 var noise = FastNoiseLite.new()
-var noise_image
+
+var tree_noise_image
+var grass_noise_image
 
 
 func _ready():
-	gen_tree_noise()
+	tree_noise_image = gen_noise_image(0.16)
+	grass_noise_image = gen_noise_image(0.05)
 	gen_ground(ground_extents)
+	#debug show noise texture
+	var texture = ImageTexture.create_from_image(grass_noise_image)
+	$"../HUD/NoiseTexture".texture = texture
 
 
 func gen_ground(extents:Vector2i):
@@ -28,25 +34,32 @@ func gen_ground(extents:Vector2i):
 			#Multiply the x and y by 2 because ground_block is 2x2.
 			var spawn_pos = start_pos + Vector3(2*x,0, 2*y)
 			var block = _block.instantiate()
-			add_child.call_deferred(block)
+			#name block
+			var name_str = "Block_" + str(x) + "_" + str(y)
+			block.name = name_str
+			add_child(block)
 			block.position = spawn_pos
 			#spawn tree
 			try_spawning_tree(block, x, y)
+			try_spawning_grass(block, x, y)
 
 
-func gen_tree_noise():
+func gen_noise_image(freq)->Image:
 	#random noise
 	noise.seed = randi()
-	noise.frequency = 0.16
+	noise.frequency = freq
 	#gen image from noise
-	noise_image = noise.get_image(ground_extents.x, ground_extents.y)
-	#debugging display
-	var texture = ImageTexture.create_from_image(noise_image)
-	$"../HUD/NoiseTexture".texture = texture
+	var img = noise.get_image(ground_extents.x, ground_extents.y)
+	return img
 
 
 func try_spawning_tree(block, x, y):
 	#print("Trying to spawn tree on block: (" + str(x) +"," + str(y) + ")")
 	#if pixel at (x,y) is dark enough, spawn tree on (x,y) block
-	if noise_image.get_pixel(x,y).r < 0.24:
+	if tree_noise_image.get_pixel(x,y).r < 0.24:
 		block.add_plant(PlantDatabase.PLANT_TYPE.TREE)
+
+
+func try_spawning_grass(block, x, y):
+	if grass_noise_image.get_pixel(x,y).r < 0.4:
+		block.make_grass()
